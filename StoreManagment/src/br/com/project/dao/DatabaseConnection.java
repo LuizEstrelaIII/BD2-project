3878b667ccd0a1af93656a84c4cd2f9c34ebb2bf
Álvaro Carrazzoni <111572599+Alvarojcb@users.noreply.github.com>
2025-05-11 10:42:24 -0300

    Update DatabaseConnection.java

--- StoreManagment/src/br/com/project/dao/DatabaseConnection.java
+++ StoreManagment/src/br/com/project/dao/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/empresa";
-    private static final String user = "root";
-    private static final String password = "@Alvin2003";
+    private static final String user = "";
+    private static final String password = "";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, user, password);
