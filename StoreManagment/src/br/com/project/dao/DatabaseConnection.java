d868a9bc12781a263afd49dd89650e3bd90b70e4
arthurbarbosaa <112869453+arthurbarbosaa@users.noreply.github.com>
2025-05-13 19:13:54 -0300

    add `getConnectionSemBanco()` to `DatabaseConnection.java`

--- StoreManagment/src/br/com/project/dao/DatabaseConnection.java
+++ StoreManagment/src/br/com/project/dao/DatabaseConnection.java
@@ -5,11 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/empresa";
+	private static final String URL = "jdbc:mysql://localhost:3306/empresa";
+	 private static final String ROOT_URL = "jdbc:mysql://localhost:3306/";
     private static final String user = "";
     private static final String password = "";
     
+    
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, user, password);
+    }
+    
+    	
+    public static Connection getConnectionSemBanco() throws SQLException {
+        return DriverManager.getConnection(ROOT_URL, user, password);
     }
 }
